e66dc23eee7cfd0a51b8864cc4db557224c176f3
Carlos Kruger <carloskr@yahoo.com>
2020-10-10 18:44:19 -0400

    solved for-in-delete

--- for-in-delete.js
+++ for-in-delete.js
@@ -41,6 +41,12 @@
 
 function showValues( obj ) {
   //Code Here
+  let newStr = '';
+  for(var key in obj) {
+    newStr+= obj[key]
+  }
+  return newStr
+
 }
 
 
@@ -54,7 +60,14 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function greaterThan10( obj ){
+  for(var key in obj){
+    if (obj[key] > 10){
+      obj[key] = 0
+    }
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -66,7 +79,12 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function double(obj){
+  for(let key in obj){
+    obj[key] = 2 * obj[key]
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -80,7 +98,16 @@ function showValues( obj ) {
 */
 
 //Code Here
+function secrets(obj){
 
+  let emptyString = '';
+  for(let key in obj){
+    if (key.startsWith('sh')){
+      emptyString = emptyString + obj[key] 
+    }
+  }
+  return emptyString
+}
 
 
 /* 
@@ -111,7 +138,10 @@ function showValues( obj ) {
 */
 
 //Code Here
-
+function removePassword(obj){
+  delete obj['password']
+  return obj
+}
 
 
 ////////// PROBLEM 6 //////////
@@ -130,7 +160,11 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+for(let key in deleteTheBigNumbers){
+  if (deleteTheBigNumbers[key] > 100){
+    delete deleteTheBigNumbers[key]
+  }
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -143,7 +177,12 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
-
+function startsWithK(obj){
+  for( var key in obj){
+    if (key.startsWith('k')) {delete obj[key]}
+  }
+  return obj
+}
 
 
 ////////// PROBLEM 8 //////////
@@ -158,5 +197,13 @@ var deleteTheBigNumbers = {
 */
 
 //Code Here
+function hiddenTreasure(obj){
+  for(var key in obj){
+    if ((obj[key].includes('treasure'))) {
+      console.log(obj[key])
+    } else {delete obj[key]}
+  }
+  return obj
+}
 
 
