acec57848ef6d0b663a8987ba167463e486fb476
Carlos Kruger <carloskr@yahoo.com>
2020-10-11 01:24:16 -0400

    nesting

--- nesting.js
+++ nesting.js
@@ -51,7 +51,19 @@ var employees = [
 */
 
 //Code Here
-
+function employeeUpdater(){
+  let removeTheoIndex = 0
+  for (let i = 0; i < employees.length; i++){
+    if(employees[i]['firstName']==="Theo"){
+      removeTheoIndex = i
+    }
+    if(employees[i]['firstName']=="Lorie"){
+      employees[i]["department"]= 'HR'
+    }
+  }
+  employees.splice(removeTheoIndex, 1)
+  return employees
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -69,7 +81,20 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 //Code Here
+function removeDuplicates(workplaceAccidents){
+  let newArray = []
+  for(var i = 0; i < workplaceAccidents.length;i++){
+    if (!newArray.includes(workplaceAccidents[i])){newArray.push(workplaceAccidents[i])}
+ }
+ workplaceAccidents.length = 0
 
+ for(var j = 0; j < newArray.length; j++)
+ {
+   workplaceAccidents.push(newArray[j])
+ }
+ 
+return workplaceAccidents
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -97,8 +122,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat['catFriends'][0]['activities'][1]
+var fluffy2ndFriend = cat['catFriends'][1]['name']
 
 
 
@@ -140,7 +165,14 @@ var myCar = {
 
 //Code Here
 
-
+function recordCleaner(myCar){
+  for (let i = 0; i < myCar['accidents'].length; i++){
+    if(myCar['accidents'][i]['atFaultForAccident']===true){
+      myCar['accidents'][i]['atFaultForAccident']=false
+    }
+  }
+  return myCar
+}
 
 ////////// PROBLEM 5 //////////
 
@@ -158,5 +190,16 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 */
 
 //Code Here
+function looper(numsArr){
+  for(let i = 0; i < numsArr.length; i++){
 
+    for(let j = 0; j < numsArr[i].length; j++){
+      if (numsArr[i][j] % 2 === 0){
+        numsArr[i][j] = 'even'
+      } else {  numsArr[i][j] = 'odd'}
+    }
+
+  }
+  return numsArr
+}
 
